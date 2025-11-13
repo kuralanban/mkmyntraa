543a752b6951f8bfe424558f40134484e947d145
yamunasree078-hue <yamunasree078@gmail.com>
2025-11-13 20:34:12 +0530

    tsk first

---
+++ myntra-mock/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import ProductCard from "./ProductCard";
+import mockData from "./mockData.json";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>🛍️ My React Store</h1>
+      <div className="product-list">
+        {mockData.map((item) => (
+          <ProductCard key={item.id} product={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
